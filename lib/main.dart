7c174094cad622f99d82cb5923dabb353fe79dcb
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-02-23 05:53:25 +0700

    ✨ feat: make fullscreen mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'app/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // Initialize Mobile Ads
-  // WidgetsFlutterBinding.ensureInitialized();
   // MobileAds.instance.initialize();
+
+  // set full screen
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
   // Initialize GetStorage
   await GetStorage.init();

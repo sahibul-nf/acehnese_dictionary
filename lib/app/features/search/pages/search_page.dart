89f3f22aa349be9bbedf63a89b5b7df8bdc9b8b0
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-03-07 02:24:40 +0700

    🐞 fix: overflow widget when keyboard showed

--- lib/app/features/search/pages/search_page.dart
+++ lib/app/features/search/pages/search_page.dart
@@ -20,182 +20,185 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 24),
-              child: SearchInput(
-                key: const Key('search_field'),
-                textController: controller.inputController,
-                onChanged: (v) {
-                  // check if not loading and typed something
-                  if (!controller.isLoading && v.isNotEmpty) {
-                    // search with delay
-                    Future.delayed(const Duration(milliseconds: 700), () {
-                      controller.search(v);
-                    });
-                  }
-                },
-                hintText: "Search word",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 24),
+                child: SearchInput(
+                  key: const Key('search_field'),
+                  textController: controller.inputController,
+                  onChanged: (v) {
+                    // check if not loading and typed something
+                    if (!controller.isLoading && v.isNotEmpty) {
+                      // search with delay
+                      Future.delayed(const Duration(milliseconds: 700), () {
+                        controller.search(v);
+                      });
+                    }
+                  },
+                  hintText: "Search word",
+                ),
               ),
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Flexible(
-                        child: SizedBox(
-                          height: 120,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .7,
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Flexible(
+                          child: SizedBox(
+                            height: 120,
+                          ),
                         ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/images/search-illustration.svg',
-                        width: MediaQuery.of(context).size.height * .25,
-                      ),
-                      const SizedBox(height: 30),
-                      AutoSizeText(
-                        "What vocabulary are \nyou looking for?",
-                        textAlign: TextAlign.center,
-                        style: AppTypography.fontStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
+                        SvgPicture.asset(
+                          'assets/images/search-illustration.svg',
+                          width: MediaQuery.of(context).size.height * .25,
                         ),
-                      ),
-                      const SizedBox(height: 16),
-                      AutoSizeText(
-                        "Find the right aceh language \nwords based on the keywords \nyou are looking for",
-                        textAlign: TextAlign.center,
-                        style: AppTypography.fontStyle(
-                          color: AppColor.secondary,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 14,
+                        const SizedBox(height: 30),
+                        AutoSizeText(
+                          "What vocabulary are \nyou looking for?",
+                          textAlign: TextAlign.center,
+                          style: AppTypography.fontStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      // selecting button for choosing algorithm type (jaro-winkler or levenshtein)
-                      Obx(
-                        () => Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Jaro-Winkler",
-                              style: AppTypography.fontStyle(
-                                fontSize: 14,
-                                color: AppColor.secondary,
+                        const SizedBox(height: 16),
+                        AutoSizeText(
+                          "Find the right aceh language \nwords based on the keywords \nyou are looking for",
+                          textAlign: TextAlign.center,
+                          style: AppTypography.fontStyle(
+                            color: AppColor.secondary,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        // selecting button for choosing algorithm type (jaro-winkler or levenshtein)
+                        Obx(
+                          () => Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Jaro-Winkler",
+                                style: AppTypography.fontStyle(
+                                  fontSize: 14,
+                                  color: AppColor.secondary,
+                                ),
                               ),
-                            ),
-                            Switch(
-                              value: !controller.isJaroWinkler.value,
-                              onChanged: (v) {
-                                controller.isJaroWinkler.value = !v;
-                              },
-                              activeColor: AppColor.primary,
-                              activeTrackColor:
-                                  AppColor.primary.withOpacity(0.2),
-                              inactiveThumbColor: AppColor.primary,
-                              inactiveTrackColor:
-                                  AppColor.primary.withOpacity(0.2),
-                            ),
-                            Text(
-                              "Levenshtein",
-                              style: AppTypography.fontStyle(
-                                fontSize: 14,
-                                color: AppColor.secondary,
+                              Switch(
+                                value: !controller.isJaroWinkler.value,
+                                onChanged: (v) {
+                                  controller.isJaroWinkler.value = !v;
+                                },
+                                activeColor: AppColor.primary,
+                                activeTrackColor:
+                                    AppColor.primary.withOpacity(0.2),
+                                inactiveThumbColor: AppColor.primary,
+                                inactiveTrackColor:
+                                    AppColor.primary.withOpacity(0.2),
                               ),
-                            ),
-                          ],
+                              Text(
+                                "Levenshtein",
+                                style: AppTypography.fontStyle(
+                                  fontSize: 14,
+                                  color: AppColor.secondary,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 20)
-                      // Obx(() {
-                      //   return admobController.isBannerAdReady.value
-                      //       ? SizedBox(
-                      //           width: admobController.bannerAd.size.width
-                      //               .toDouble(),
-                      //           height: admobController.bannerAd.size.height
-                      //               .toDouble(),
-                      //           child: AdWidget(ad: admobController.bannerAd),
-                      //         )
-                      //       : const SizedBox();
-                      // })
-                    ],
-                  ),
-                  Obx(() {
-                    return controller.recommendations.isEmpty ||
-                            controller.inputController.text.isEmpty
-                        ? const SizedBox()
-                        : Container(
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 24, vertical: 4),
-                            // height: 140,
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 8,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(.7),
-                              borderRadius: BorderRadius.circular(25),
-                              boxShadow: [
-                                BoxShadow(
-                                    offset: const Offset(12, 26),
-                                    blurRadius: 50,
-                                    spreadRadius: 0,
-                                    color: Colors.grey.withOpacity(.1)),
-                              ],
-                            ),
-                            child: (controller.isLoading)
-                                ? SizedBox(
-                                    height: 40,
-                                    child: Center(
-                                      child: LoadingAnimationWidget
-                                          .prograssiveDots(
-                                        color: AppColor.secondary ,
-                                        size: 30,
+                        const SizedBox(height: 20)
+                        // Obx(() {
+                        //   return admobController.isBannerAdReady.value
+                        //       ? SizedBox(
+                        //           width: admobController.bannerAd.size.width
+                        //               .toDouble(),
+                        //           height: admobController.bannerAd.size.height
+                        //               .toDouble(),
+                        //           child: AdWidget(ad: admobController.bannerAd),
+                        //         )
+                        //       : const SizedBox();
+                        // })
+                      ],
+                    ),
+                    Obx(() {
+                      return controller.recommendations.isEmpty ||
+                              controller.inputController.text.isEmpty
+                          ? const SizedBox()
+                          : Container(
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 4),
+                              // height: 140,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 8,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(.7),
+                                borderRadius: BorderRadius.circular(25),
+                                boxShadow: [
+                                  BoxShadow(
+                                      offset: const Offset(12, 26),
+                                      blurRadius: 50,
+                                      spreadRadius: 0,
+                                      color: Colors.grey.withOpacity(.1)),
+                                ],
+                              ),
+                              child: (controller.isLoading)
+                                  ? SizedBox(
+                                      height: 40,
+                                      child: Center(
+                                        child: LoadingAnimationWidget
+                                            .prograssiveDots(
+                                          color: AppColor.secondary,
+                                          size: 30,
+                                        ),
                                       ),
+                                    )
+                                  : Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        for (var item
+                                            in controller.recommendations)
+                                          InkWell(
+                                            onTap: () {
+                                              // set input text
+                                              controller.inputController.text =
+                                                  item.aceh;
+
+                                              // reset recommendations
+                                              controller.resetRecommendations();
+
+                                              // if (admobController
+                                              //     .isInterstitialAdReady.value) {
+                                              //   admobController.interstitialAd?.show();
+                                              // }
+
+                                              // navigate to word detail page
+                                              Get.toNamed(
+                                                AppRoutes.wordDetail,
+                                                arguments: item.id,
+                                              );
+                                            },
+                                            child: RecommendationCard(
+                                              textAceh: item.aceh,
+                                              similiarity: item.similiarity
+                                                  .toStringAsFixed(2),
+                                            ),
+                                          )
+                                      ],
                                     ),
-                                  )
-                                : Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      for (var item
-                                          in controller.recommendations)
-                                        InkWell(
-                                          onTap: () {
-                                            // set input text
-                                            controller.inputController.text =
-                                                item.aceh;
-
-                                            // reset recommendations
-                                            controller.resetRecommendations();
-
-                                            // if (admobController
-                                            //     .isInterstitialAdReady.value) {
-                                            //   admobController.interstitialAd?.show();
-                                            // }
-
-                                            // navigate to word detail page
-                                            Get.toNamed(
-                                              AppRoutes.wordDetail,
-                                              arguments: item.id,
-                                            );
-                                          },
-                                          child: RecommendationCard(
-                                            textAceh: item.aceh,
-                                            similiarity: item.similiarity
-                                                .toStringAsFixed(2),
-                                          ),
-                                        )
-                                    ],
-                                  ),
-                          );
-                  }),
-                ],
-              ),
-            )
-          ],
+                            );
+                    }),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
